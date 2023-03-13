a7db0e2c4f90f6f47ba416bc13fc5a6bfa7df55a
Nikolaipe <nikolaipe@getacademy.no>
2023-03-13 13:06:44 +0100

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿namespace Finn_meg_en_hobby
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int minRandom = 0;
+            int maxRandom = 5;
+            string person = "bruker";
+            Console.WriteLine("Hvem har lyst på hobby?");
+            while (!string.IsNullOrWhiteSpace(person))
+            {
+                person = Console.ReadLine();
+                int randomNum = randomizer(minRandom, maxRandom);
+                string[] hobbies = new string[5] { "bli gamer", "strikke genser", "lære koding", "trene til maraton", "drive strikkhopp"};
+                Console.WriteLine("Ja, " + person + " kan jo kanskje " + hobbies[randomNum] + "?");
+            }
+
+        }
+
+        private static int randomizer(int minRandom, int maxRandom)
+        {
+            Random rnd = new Random();
+            int num = rnd.Next(minRandom, maxRandom);
+            return num;
+        }
+    }
+}
